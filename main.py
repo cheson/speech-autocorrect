5540f3d4a2c2728feb323c5edc6636ac129fca05
Gloria Chua <cjmg2006@gmail.com>
2016-12-05 11:40:08 -0800

    Add files via upload
    updated with evaluation functions

--- main.py
+++ main.py
@@ -2,38 +2,70 @@ import newBigram
 from cutWavFiles import ProcessAlignments
 from reconstruct import Reconstruct
 from cutWavFiles import ALPHABET
+import evaluate
+
 
 # gets bigramCost, possibleFills based on corpus
 CORPUS_PATH = 'corpus/'
-TRANSCRIPT_FILE = 'data/AllText.txt'
-
 bigramCost, possibleFills = newBigram.getRealCosts(CORPUS_PATH)
+TRANSCRIPT_FILE = 'data/AllText.txt'
 
 # processes wav files as classes
 toReconstruct = Reconstruct()
 alignment = ProcessAlignments()
 monologueAllCutTimes = alignment.getCutTimes('monologue', ALPHABET)
 
+totalPercentageChange = 0
+totalNumQueries = 0 
+totalImprovements = 0
 queries = alignment.produceStrings(monologueAllCutTimes) # output for Gloria in string format, list of strings
 
 
 
-for filePrefix in queries:
-    query = queries[filePrefix] #for each query == for each monologue
-    # vowelsToFill is a list of vowels in sequence
-    print query 
-    vowelsToFill = newBigram.run(query, bigramCost, possibleFills) # need to edit run to return list of vowels
-    finalConcatWavFile = 'data/finalOutput/' + filePrefix + 'reconstructed.wav'
-    toReconstruct.fullReconstruct(filePrefix, 'monologue', vowelsToFill, finalConcatWavFile)
+# for filePrefix in queries:
+#     query = queries[filePrefix] #for each query == for each monologue
+#     # vowelsToFill is a list of vowels in sequence
+#     print query 
+#     vowelsToFill = newBigram.run(query, bigramCost, possibleFills) # need to edit run to return list of vowels
+#     finalConcatWavFile = 'data/finalOutput/' + filePrefix + 'reconstructed.wav'
+#     toReconstruct.fullReconstruct(filePrefix, 'monologue', vowelsToFill, finalConcatWavFile)
 	
-	transcript = ''
+	
+	# eval <- wav1, wav2
+	# eval <- wav, transcript 
+
+	# extract line with same starting filePrefix in data/AllText.txt 
+	transcript = ""
 	with open(TRANSCRIPT_FILE) as f:
-	for line in f:
-		if line.find(filePrefix) == 0:
-		     transcript = line
-		     transcript = transcript.replace(filePrefix, '')
+    for line in f:
+        if line.find(filePrefix) == 0:
+             transcript = line
+             transcript = transcript.replace(filePrefix, '')
 
-	#modifiedWav = filePrefix + modifiedWavLocation
 
-	#call eval(modifiedWav, transcript)
-	#os.system('call eval') eval(inputWav, transcript, filePrefix)
+    
+    originalWavFile = 'data/audio/' + filePrefix + '.wav'
+    origScore = evaluate.main(originalWavFile, transcript, filePrefix)
+    modifiedScore = evaluate.main(finalConcatWavFile, transcript, filePrefix)
+    print "Original score: " + origScore + "; Modified score: " + modifiedScore
+    percentageChange = (modifiedScore - origScore) * 1.0 / origScore * 100
+    improvement = (percentageChange > 0)
+    print "Percentage improvement: " + percentageChange + "; Improvement: " + improvement
+
+    totalPercentageChange += percentageChange
+    if improvement == 1:
+    	totalImprovements += 1
+    totalNumQueries += 1
+
+avPercentageChange = totalPercentageChange * 1.0 / totalNumQueries
+avImprovement = totalImprovements * 1.0 / totalNumQueries 
+
+
+	# string for 
+	modifiedWav = path;
+	transcriptWav = path;
+
+	# for each query, call eval on result and its monologue transcript
+
+query = "s* p**d*"
+newBigram.run(query, bigramCost, possibleFills)
